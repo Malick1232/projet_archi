1d10db94b536e4a5ee661e0b6ba05d8ef2544ba2
Malick1232 <maluckdiallo@gmail.com>
2026-07-22 04:37:09 +0200

    modification des erreur sur les modification de lediteur

--- src/main/java/com/siteactualites/config/WebConfig.java
+++ src/main/java/com/siteactualites/config/WebConfig.java
@@ -2,7 +2,7 @@ package com.siteactualites.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.accept.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
